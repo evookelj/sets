7d4dc2868bac8790baf3a73a21ca77b4bad4e6e1
evookelj <evukelj@stuy.edu>
2017-04-24 12:22:54 -0400

    add some tests

--- comprendo.py
+++ comprendo.py
@@ -12,3 +12,13 @@ def set_diff(a,b):
 
 def cart_prod(a,b):
 	return [(a[i],b[i]) for i in range(len(a))]
+
+a = [1,3,5,7,9]
+b = [0,2,4,6,8]
+c = union(a,b)
+print "c = union [1,3,5,7,9] and [0,2,4,6,8] = %s"%(str(c))
+
+d = intersect(b,c)
+print "d = intersection [0,2,4,6,8] and c = %s"%(str(d))
+
+e = 
